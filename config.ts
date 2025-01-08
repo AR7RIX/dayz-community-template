db6db448fe5e37136f389ff9583ec09f7abbc180
AR7RIX <qrthur@gmail.com>
2025-01-08 22:41:17 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -62,14 +62,14 @@ export const config: Config = {
           title: "Leaderboard",
           description: "Check out the top players on our servers and see how you compare. Can you make it to the top of the leaderboard?",
         },
-/*         contact: {
+        contact: {
           title: "Contact Us",
           description: "If you have any questions or concerns, please don't hesitate to contact us through the following channels.",
           links: {
             'Discord': "https://discord.gg/bastiondayz",
             'Email': "mailto:info@Bastion.eu",
           },
-        }, */
+        },
       },
     },
   },
